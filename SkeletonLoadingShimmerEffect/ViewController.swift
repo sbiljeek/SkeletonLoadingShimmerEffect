a5729b82783fe2d56586979b0ef95b70eec41d9c
sbiljeek <sbiljeekdev@gmail.com>
2023-03-12 12:48:27 +0300

    Initial Commit

---
+++ SkeletonLoadingShimmerEffect/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SkeletonLoadingShimmerEffect
+//
+//  Created by Salman Biljeek on 3/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
